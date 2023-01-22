c0e421230a7cde92cefd8ab2fee55188549d386d
Pálio <paulo.tarso@escolar.ifrn.edu.br>
2023-01-22 16:24:52 -0300

    .

--- Listagem/admin.py
+++ Listagem/admin.py
@@ -1,4 +1,7 @@
-from django.contrib import admin
-from Listagem       import models
+from django.contrib     import admin
+from Listagem.models    import Musica
+
 # Register your models here.
-admin.site.register(models.Musica)
+@admin.register(Musica)
+class MusicaAdmin(admin.ModelAdmin):
+    list_display = ['mu_id', 'mu_musica', 'mu_autor', 'mu_interprete', 'mu_capa']
