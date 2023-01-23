d16fc6f9d7a637b2c167a9d19beefa848b5f981b
Pálio <paulo.tarso@escolar.ifrn.edu.br>
2023-01-23 02:24:11 -0300

    Autenticação na LISTAGEM adicionada como TESTE

--- Listagem/forms.py
+++ Listagem/forms.py
@@ -1,7 +1,15 @@
-from django.forms   import ModelForm
-from Listagem       import models
+from django.contrib.auth.forms  import UserCreationForm
+from django.contrib.auth.models import User
+from django                     import forms
+from django.forms               import ModelForm
+from Listagem                   import models
 
 class MusicaForm(ModelForm):
     class Meta:
         model = models.Musica
         fields = '__all__'
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1']
--- Listagem/views.py
+++ Listagem/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts          import render, redirect
+from django.http               import HttpResponse
 from Listagem                  import models, forms
 from django.contrib.auth.forms import UserCreationForm
 
+#Create your views here.
 def index(request):
     return render(request, "index.html")
 
@@ -10,6 +12,12 @@ def login(request):
 
 def register(request):
     form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
     context = {'form': form}
     return render(request, "registration/register.html")
 
