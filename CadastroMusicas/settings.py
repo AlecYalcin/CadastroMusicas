cb9f558ed6e9838165d20dad2368138776ea8ec7
Alec Yalcin <alec.yalcin@escolar.ifrn.edu.br>
2023-01-30 13:42:49 -0300

    Branch para servidor, com MYSQL

--- CadastroMusicas/settings.py
+++ CadastroMusicas/settings.py
@@ -63,18 +63,18 @@ WSGI_APPLICATION = 'CadastroMusicas.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'db_musicas',
-    #     'USER': 'user_musicas',
-    #     'PASSWORD': '12345',
-    #     'HOST': 'localhost',
-    #     'PORT': '3306',
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_musicas',
+        'USER': 'user_musicas',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
 }
 
 
